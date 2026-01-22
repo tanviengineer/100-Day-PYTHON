d5a085aef76feb5193ccfad2a4627bf6241e6ba1
tanviengineer <tanviengineer2005@gmail.com>
2026-01-22 22:00:09 +0530

    understand python

--- day21_function_argument.py
+++ day21_function_argument.py
@@ -1,2 +1,11 @@
+def add(a,b):
+    print(a+b)
+x= int (input("Enter the first value : "))
+y= int (input("Enter the first value : "))
+add(x,y)
 
- 
+def sub(a,b=10):
+    print(a-b)
+t= int (input("Enter the first value : "))
+q= int (input("Enter the first value : "))
+sub(t,q)
---
+++ day22_list.py
@@ -0,0 +1,4 @@
+# LIST = list is a collection of elements 
+
+list1 = [10,20,30,40,50]
+print(type(list1))
