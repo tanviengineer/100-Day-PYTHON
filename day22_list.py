c65244de4f34ec4af191999a5c85cba23faea4d3
tanviengineer <tanviengineer2005@gmail.com>
2026-01-24 20:55:13 +0530

    understand python

--- day22_list.py
+++ day22_list.py
@@ -2,3 +2,22 @@
 
 list1 = [10,20,30,40,50]
 print(type(list1))
+
+L1 = [ 1,2,3,4,5,"tanvi","naval"]
+print(L1)
+print(type(L1))
+L1.append("Joy")
+print("Add item in list    : ",L1)
+L1.remove("Joy")
+print("Remove Item in List : ",L1)
+
+T1 = (1,2,3,4,5,6,"Hello", "World")
+print(T1)
+print(type(T1))
+
+d1={"Name":"Tanvi","City":"Kolkata","Course":"Data Science" }
+print(d1)
+print(type(d1))
+print(d1["Name"])
+d1["Course"] = "Artificial Intelligence"
+print(d1)
---
+++ day23_tuple.py
@@ -0,0 +1,3 @@
+T1 = (1,2,3,4,5,6,"Hello", "World")
+print(T1)
+print(type(T1))
