47982409425193236f34d79cfd0879e345a5cc31
tanviengineer <tanviengineer2005@gmail.com>
2026-01-25 21:46:52 +0530

    understand python

--- day24_dictionary.py
+++ day24_dictionary.py
@@ -2,4 +2,6 @@ dic1 ={"name":"tanvi","class":"msc ai ","roll no ": "25AI23"}
 print(dic1)
 print(type(dic1))
 print(dic1="name")
-dic2 ={""}
+
+dic2 = {1 :"monday", 2: "tuesday",3: "wednesday",4:"thuesday",5:"friday",6:"satuerday",7:"sunday"}
+print (dic2)
