7ca57f442c2fe1d565dfab54a7063b720d7ad3d0
tanviengineer <tanviengineer2005@gmail.com>
2026-01-29 20:20:48 +0530

    understand python

--- day14_if-else.py
+++ day14_if-else.py
@@ -1,14 +1,12 @@
 #  if else statement 
 # IDEs in python ?
-age = int(input("Enter ypur age : "))
+age = int(input("Enter your age : "))
 if age >=18:
     print("you are eligible")
 else :
     print("you are not eligible")
-
 #  if elif else statement
 a = int(input("enter a  value : "))
-
 # nested if else statement
 a= "\nchecking you are eligible or not for voting \n "
 print(a)
@@ -22,9 +20,6 @@ if ages >= 18:
         print("You are not citizen , so you are not eligible for voting")
 else: 
     print("You are under age ,you are not eligible for voting ")
-
-
-
 # if elif else statement
 b= "\nchecking the day of week of September  using date\n "
 print(b)
@@ -39,9 +34,10 @@ elif date <=31 and date%7 ==3:
     print("Today is TUESDAY")
 elif date <=31 and  date%7==4:
     print("Today is WEDNESDAY")
-elif date <=31 and date%7==5:
+elif date <=31 and date%7==5: 
     print("Today is THUESDAY ")
 elif date <=31 and date%7 ==6:
     print("Today is FRIDAY")
 else :
     print("Given date is invalid ")
+
--- day24_dictionary.py
+++ day24_dictionary.py
@@ -1,7 +1,8 @@
 dic1 ={"name":"tanvi","class":"msc ai ","roll no ": "25AI23"}
 print(dic1)
 print(type(dic1))
-print(dic1="name")
 
 dic2 = {1 :"monday", 2: "tuesday",3: "wednesday",4:"thuesday",5:"friday",6:"satuerday",7:"sunday"}
-print (dic2)
+print (dic2) 
+
+dic3 = {1:"tanvi",2:"jamnyal"}
